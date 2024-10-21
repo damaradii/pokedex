59da1bf9eb23bd386821199fe333935c7a4c0615
Damar Adi N <damaradii.nugroho@gmail.com>
2024-10-21 19:58:35 +0700

    update backend 100 data

--- backend/generateJsonDB.js
+++ backend/generateJsonDB.js
@@ -6,7 +6,7 @@ async function generateJsonDB() {
     // json file bernama db.json. pastikan ketika kalian menjalankan npm run start
     // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
     // pokemon yang telah kalian parsing dari public api pokemon
-    const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=10";
+    const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
     const pokemonList = await fetch(pokemonApiURL).then((res) => res.json());
     const payload = [];
     for (let i = 0; i < pokemonList.results.length; i++) {
